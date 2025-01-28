237a1050ec813308310d49aed04c3a542f5a38df
Bruno_Iannone <iannone.1883472@studenti.uniroma1.it>
2025-01-28 23:22:04 +0100

    Almost finished sorting function for YoLo

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import json
 import os
 import shutil
 
+
 def get_bbox_from_json(json_file):
     """Create a list of dictionaries containing bounding box coordinates for each valid sample. Invalid samples are represented with a 0. An example: [{'X': 112.28, 'Y': 188.28, 'W': 293.44, 'H': 293.44},0]
 
@@ -40,12 +41,13 @@ def get_bbox_from_json(json_file):
 
     return valid_boxes
 
+
 def bbox_coord_from_dict(bbox_dict):
     x = int(bbox_dict['X'])
     y = int(bbox_dict['Y'])
     w = int(bbox_dict['W'])
     h = int(bbox_dict['H'])
-    return x,y,w,h
+    return x, y, w, h
 
 
 def draw_bounding_boxes_from_json(f_json_file, l_json_file, r_json_file, images_dir, show):
@@ -129,7 +131,9 @@ def draw_bounding_boxes_from_json(f_json_file, l_json_file, r_json_file, images_
         else:
             print(f"Failed to read {img}.")
 
-def draw_bounding_boxes_from_list(f_list,images_dir,show):#f_json_file, l_json_file, r_json_file, images_dir, show):
+
+# f_json_file, l_json_file, r_json_file, images_dir, show):
+def draw_bounding_boxes_from_list(f_list, images_dir, show):
     """Draw the bounding boxes over all images of a sample. Json files contain X and Y coordinates, Width (W) and Height (H) of the bounding box and a binary list of valid samples.
 
     Args:
@@ -143,13 +147,14 @@ def draw_bounding_boxes_from_list(f_list,images_dir,show):#f_json_file, l_json_f
     Raises:
         ValueError: raise an error when the number of images and bounding boxes mismatch.
     """
-    #f_data = get_bbox_from_json(f_list)
+    # f_data = get_bbox_from_json(f_list)
 
    # l_eye_data = get_bbox_from_json(l_json_file)
 
-    #r_eye_data = get_bbox_from_json(r_json_file)
+    # r_eye_data = get_bbox_from_json(r_json_file)
 
-    images = sorted([img for img in os.listdir(images_dir)]) #RICORDATI DI MODIFICARE
+    # RICORDATI DI MODIFICARE
+    images = sorted([img for img in os.listdir(images_dir)])
 
     # if len(f_data) != len(images) or len(l_eye_data) != len(images) or len(r_eye_data) != len(images):
     #     raise ValueError(
@@ -211,6 +216,7 @@ def draw_bounding_boxes_from_list(f_list,images_dir,show):#f_json_file, l_json_f
         else:
             print(f"Failed to read {img}.")
 
+
 def sort_samples(images_dir, dest_dir):
     """Util function to sort images in train,val and test set. Note: dest_dir must be outside images_dir
 
@@ -219,14 +225,15 @@ def sort_samples(images_dir, dest_dir):
         dest_dir (str): Destination path
 
     """
-    samples = sorted([sample_folder for sample_folder in os.listdir(images_dir) if os.path.isdir(os.path.join(images_dir, sample_folder))])
-    
+    samples = sorted([sample_folder for sample_folder in os.listdir(
+        images_dir) if os.path.isdir(os.path.join(images_dir, sample_folder))])
+
     for i, img in enumerate(samples):
         print(img)
-        
+
         with open(images_dir + img + "/info.json", 'r') as f:
             data = json.load(f)
-        
+
         set = data["Dataset"]
 
         if set == "test":
@@ -236,38 +243,104 @@ def sort_samples(images_dir, dest_dir):
         else:
             shutil.move(images_dir + img, dest_dir + "train")
 
+
 def bbox_dict_to_list_of_list(bbox_dict):
     bbox_list = []
     for dict in bbox_dict:
         if dict == 0:
             bbox_list.append(0)
         else:
-            bbox_list.append(list(dict.values())) 
+            bbox_list.append(list(dict.values()))
 
     return bbox_list
 
-def convert_gazecapture_for_yolo(src_folder,dest_folder):
+
+def coco_to_yolo_bbox_converter(bbox_list, image_width, image_height):
+    """
+    Converte una lista di bounding box dal formato COCO al formato YOLO.
+
+    Args:
+        bboxes (list of tuples): Lista di bounding box in formato COCO (x_min, y_min, width, height).
+        image_width (int): Larghezza dell'immagine.
+        image_height (int): Altezza dell'immagine.
+
+    Returns:
+        list of tuples: Lista di bounding box in formato YOLO (x_center, y_center, width, height).
+    """
+    yolo_bboxes = []
+    for bbox in bbox_list:
+        x_min, y_min, width, height = bbox
+
+        # Calcolo centro della bounding box
+        x_center = x_min + width / 2.0
+        y_center = y_min + height / 2.0
+
+        # Normalizzazione rispetto alla dimensione dell'immagine
+        x_center /= image_width
+        y_center /= image_height
+        width /= image_width
+        height /= image_height
+
+        # Aggiungi la bounding box YOLO alla lista
+        yolo_bboxes.append([x_center, y_center, width, height])
+
+    return yolo_bboxes
+
+
+def write_list(elem_list, dest_folder, file_name):
+
+    with open(os.path.join(dest_folder, file_name + ".txt"), "a") as file:
+        # Convert elem_list to a list of values
+        elem_list = list(elem_list.values())
+
+        # Join the elements with a space to avoid trailing space
+        line = " ".join(map(str, elem_list))
+
+        # Write the line followed by a newline
+        file.write(line + "\n")
+
+
+def convert_gazecapture_for_yolo(src_folder, dest_folder):
+    os.makedirs(os.path.join(src_folder, "test"),exist_ok=True)
+    os.makedirs(os.path.join(src_folder, "val"),exist_ok=True)
+    os.makedirs(os.path.join(src_folder, "train"),exist_ok=True)
+    to_avoid_dirs = ["train", "val", "test"]
+
     for persona in os.listdir(src_folder):
 
-        f_data = get_bbox_from_json(os.path.join(src_folder,persona, 'appleFace.json'))
+        if persona in to_avoid_dirs:
+            continue
 
-        l_eye_data = get_bbox_from_json(os.path.join(src_folder,persona, 'appleLeftEye.json'))
+        f_data = get_bbox_from_json(os.path.join(
+            src_folder, persona, 'appleFace.json'))
 
-        r_eye_data = get_bbox_from_json(os.path.join(src_folder,persona, 'appleRightEye.json'))
+        l_eye_data = get_bbox_from_json(os.path.join(
+            src_folder, persona, 'appleLeftEye.json'))
+
+        r_eye_data = get_bbox_from_json(os.path.join(
+            src_folder, persona, 'appleRightEye.json'))
+        with open(os.path.join(src_folder, persona, "info.json"), 'r') as f:
+            destination_set = json.load(f)
 
         if len(f_data) != len(r_eye_data) or len(f_data) != len(l_eye_data):
             raise ValueError(
                 f"Dimension mismatch: data has {len(f_data)} items, left eye data has {len(l_eye_data)}, right eye data has {len(r_eye_data)}")
-        
-        for i,image in enumerate(os.listdir(os.path.join(src_folder, persona , "frames"))):
-            
-            src_image_path = os.path.join(src_folder, persona , "frames")
-            new_name = persona + "_"  + str(i) + "."  +  "jpg"
-                        
-            os.rename(os.path.join(src_image_path, image), os.path.join(src_image_path, new_name)) 
-            shutil.copy(os.path.join(src_image_path, new_name), dest_folder )
 
+        samples = sorted([sample_folder for sample_folder in os.listdir(
+            os.path.join(src_folder, persona, "frames"))])
 
+        for i, image in enumerate(samples):
+            if f_data[i] == 0 or l_eye_data[i] == 0 or r_eye_data[i] == 0:
+                print("Invalid bbox", persona, image)
+                continue
 
+            src_image_path = os.path.join(src_folder, persona, "frames", image)
+            new_name = persona + "_" + str(i) + "." + "jpg"
+            dest_folder = os.path.join(src_folder , destination_set["Dataset"])
+            shutil.copy(src_image_path, dest_folder)
+            os.rename(os.path.join(dest_folder, image),
+                      os.path.join(dest_folder, new_name))
 
-
+            write_list(f_data[i], dest_folder, new_name.split(".")[0])
+            write_list(l_eye_data[i], dest_folder, new_name.split(".")[0])
+            write_list(r_eye_data[i], dest_folder, new_name.split(".")[0])
